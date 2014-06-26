3441c58dbb17ef440d97bcc2bbac9e074d051c99
taojonesin <taojonesin (at) nowhere.com>
2014-06-25 19:51:03 -0700

    added some routing

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,25 @@
+var url = require('url');
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var helpers = require('./http-helpers');
 // require more modules/folders here!
 
+var routes = {
+  '/' : helpers.sendIndex,
+  '/loading.html' : helpers.sendLoading,
+  '/styles.css' : helpers.sendStyles
+};
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var path = url.parse(req.url).pathname;
+  var method = req.method;
+
+  if (method ==='OPTIONS') {
+    helpers.sendOptionsResponse(req, res);
+  } else if (path && routes[path]) {
+    routes[path](req, res);
+  } else {
+    helpers.send404(req, res);
+  }
+  // res.end(archive.paths.list);
 };
